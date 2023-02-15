ac34bb35c3b11020d6d1b1fc44f326310a13a55b
MasKoding <masdidi00@gmail.com>
2023-02-15 14:54:26 +0700

    image

--- header.php
+++ header.php
@@ -10,5 +10,39 @@
 
     <title>BlogKuh</title>
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body style="background-color: #222;">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">BlogKuh</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="main.php?page=about">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kategori
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Berita</a></li>
+            <li><a class="dropdown-item" href="#">Olahraga</a></li>
+            <li><a class="dropdown-item" href="#">Movie</a></li>
+  
+            <li><a class="dropdown-item" href="#">Lainnya</a></li>
+          </ul>
+        </li>
+    
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
--- home.php
+++ home.php
@@ -1,1 +1,15 @@
-<h1>Ini Halaman Home</h1>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-12">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">News</h5>
+                        <hr>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
